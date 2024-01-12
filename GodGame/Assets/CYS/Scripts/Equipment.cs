ca6a0fa4c01c9a8a5fdf50848139eecff18fc64c
Chronos410 <chronos410@naver.com>
2024-01-12 19:12:41 +0900

    add: PrivateFolder

---
+++ GodGame/Assets/CYS/Scripts/Equipment.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Equipment
+{
+    public class Equip : MonoBehaviour
+    {
+        public string equipName;
+        public int atk;
+        public int ap;
+        public int spd;
+    }
+}
+
